ac3952b5ef5dd7148b6a4e7554496fe2f90f3ee4
forsaken <forsaken@shadow.lan>
2023-04-20 06:31:37 -0400

    removed website from this git

--- website/src/common/components/NoSSR.js
+++
@@ -1,8 +0,0 @@
-import { Fragment } from 'react';
-import dynamic from 'next/dynamic';
-
-const NoSSR = ({ children }) => <Fragment>{children}</Fragment>;
-
-export default dynamic(() => Promise.resolve(NoSSR), {
-  ssr: false,
-});
